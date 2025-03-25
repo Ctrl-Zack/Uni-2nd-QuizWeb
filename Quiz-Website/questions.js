7413bc6a80b827be5c3b854b47647cbfcf7b83d0
Zaky Zein <60463403+Ctrl-Zack@users.noreply.github.com>
2025-03-25 22:31:26 +0700

    Add files via upload

---
+++ Quiz-Website/questions.js
@@ -0,0 +1,277 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Menurut Teori Gujarat, Islam masuk ke Indonesia melalui pedagang dari:",
+        answer: "C. India",
+        options: [
+            "A. Persia",
+            "B. Arab",
+            "C. India",
+            "D. Tiongkok"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Kerajaan Islam pertama di Indonesia adalah:",
+        answer: "C. Kerajaan Samudera Pasai",
+        options: [
+            "A. Kesultanan Aceh",
+            "B. Kesultanan Demak",
+            "C. Kerajaan Samudera Pasai",
+            "D. Kerajaan Mataram"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Pendiri Kerajaan Samudera Pasai adalah:",
+        answer: "C. Meurah Silu",
+        options: [
+            "A. Sultan Agung",
+            "B. Raden Patah",
+            "C. Meurah Silu",
+            "D. Sultan Iskandar Muda"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Pada masa kejayaannya, Samudera Pasai menggunakan mata uang yang disebut:",
+        answer: "B. Dirham",
+        options: [
+            "A. Real",
+            "B. Dirham",
+            "C. Dinar",
+            "D. Koin Perak"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Kerajaan Aceh mencapai puncak kejayaannya pada masa pemerintahan:",
+        answer: "B. Sultan Iskandar Muda",
+        options: [
+            "A. Sultan Ali Mughayat Syah",
+            "B. Sultan Iskandar Muda",
+            "C. Sultan Malik Al-Saleh",
+            "D. Sultan Alaidin Righayat Syah"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Kesultanan Jambi mengalami kejayaan pada abad ke-17 karena perdagangan:",
+        answer: "B. Lada",
+        options: [
+            "A. Rempah-rempah",
+            "B. Lada",
+            "C. Kapur Barus",
+            "D. Kain Sutra"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Sultan pertama dari Kesultanan Demak adalah:",
+        answer: "B. Raden Patah",
+        options: [
+            "A. Sultan Trenggono",
+            "B. Raden Patah",
+            "C. Pati Unus",
+            "D. Joko Tingkir"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Kesultanan Mataram Islam mencapai puncak kejayaannya pada masa pemerintahan:",
+        answer: "B. Sultan Agung",
+        options: [
+            "A. Panembahan Senopati",
+            "B. Sultan Agung",
+            "C. Amangkurat I",
+            "D. Pakubuwana II"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Kesultanan Cirebon memiliki peran penting dalam penyebaran Islam di Jawa Barat berkat:",
+        answer: "B. Sunan Gunung Jati",
+        options: [
+            "A. Sultan Trenggono",
+            "B. Sunan Gunung Jati",
+            "C. Pangeran Walangsungsang",
+            "D. Sultan Agung"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Kerajaan Islam di Kalimantan yang pertama kali berdiri adalah:",
+        answer: "A. Kerajaan Banjar",
+        options: [
+            "A. Kerajaan Banjar",
+            "B. Kesultanan Kutai Kertanegara",
+            "C. Kesultanan Pontianak",
+            "D. Kesultanan Pasir"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Islam masuk ke Nusantara pada abad ke-9 menurut teori:",
+        answer: "D. Tiongkok",
+        options: [
+            "A. Gujarat",
+            "B. Persia",
+            "C. Arab",
+            "D. Tiongkok"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Teori yang menyatakan bahwa Islam masuk ke Indonesia langsung dari Arab adalah:",
+        answer: "D. Teori Arab",
+        options: [
+            "A. Teori Gujarat",
+            "B. Teori Persia",
+            "C. Teori Tiongkok",
+            "D. Teori Arab"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Kerajaan Samudera Pasai berkembang pesat sebagai pusat perdagangan internasional karena mengekspor:",
+        answer: "B. Lada dan emas",
+        options: [
+            "A. Kapur barus dan teh",
+            "B. Lada dan emas",
+            "C. Beras dan kayu jati",
+            "D. Gula dan garam"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Sultan Malik Al Saleh adalah raja pertama dari:",
+        answer: "C. Samudera Pasai",
+        options: [
+            "A. Kerajaan Aceh",
+            "B. Kesultanan Demak",
+            "C. Samudera Pasai",
+            "D. Kerajaan Banjar"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Kesultanan Aceh menguasai perdagangan di Selat Malaka dan berhasil menaklukkan:",
+        answer: "A. Pahang",
+        options: [
+            "A. Pahang",
+            "B. Malaka",
+            "C. Palembang",
+            "D. Banten"
+        ]
+    },
+    {
+        numb: 16,
+        question: "Mata uang yang digunakan pada masa Kesultanan Aceh disebut:",
+        answer: "C. Dirham",
+        options: [
+            "A. Rupiah",
+            "B. Real",
+            "C. Dirham",
+            "D. Dinar"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Kesultanan Jambi mencapai puncak kejayaannya pada abad ke-17 karena:",
+        answer: "B. Menghasilkan lada dalam jumlah besar",
+        options: [
+            "A. Memiliki banyak tambang emas",
+            "B. Menghasilkan lada dalam jumlah besar",
+            "C. Bersekutu dengan VOC",
+            "D. Menguasai jalur perdagangan rempah"
+        ]
+    },
+    {
+        numb: 18,
+        question: "Kesultanan Demak didirikan oleh:",
+        answer: "A. Raden Patah",
+        options: [
+            "A. Raden Patah",
+            "B. Sunan Gunung Jati",
+            "C. Sultan Trenggono",
+            "D. Sultan Agung"
+        ]
+    },
+    {
+        numb: 19,
+        question: "Penyebaran Islam di Cirebon sangat dipengaruhi oleh:",
+        answer: "C. Sunan Gunung Jati",
+        options: [
+            "A. Sunan Kalijaga",
+            "B. Sunan Muria",
+            "C. Sunan Gunung Jati",
+            "D. Sunan Giri"
+        ]
+    },
+    {
+        numb: 20,
+        question: "Wali Songo berperan penting dalam menyebarkan Islam di wilayah:",
+        answer: "B. Jawa",
+        options: [
+            "A. Sumatera",
+            "B. Jawa",
+            "C. Kalimantan",
+            "D. Sulawesi"
+        ]
+    },
+    {
+        numb: 21,
+        question: "Kerajaan Islam pertama di Kalimantan Selatan adalah:",
+        answer: "A. Kesultanan Banjar",
+        options: [
+            "A. Kesultanan Banjar",
+            "B. Kesultanan Pontianak",
+            "C. Kesultanan Kutai",
+            "D. Kesultanan Pasir"
+        ]
+    },
+    {
+        numb: 22,
+        question: "Kesultanan Kutai Kertanegara berkembang pesat pada masa pemerintahan:",
+        answer: "D. Sultan AM Sulaiman",
+        options: [
+            "A. Sultan Aji Muhammad Salehuddin",
+            "B. Sultan Agung",
+            "C. Sultan Mustasin Billah",
+            "D. Sultan AM Sulaiman"
+        ]
+    },
+    {
+        numb: 23,
+        question: "Perdagangan di Kalimantan berkembang pesat karena komoditas utama seperti:",
+        answer: "B. Lada dan intan",
+        options: [
+            "A. Padi dan karet",
+            "B. Lada dan intan",
+            "C. Rotan dan tembakau",
+            "D. Kopi dan gula"
+        ]
+    },
+    {
+        numb: 24,
+        question: "Kerajaan Islam di Papua yang tunduk pada Kesultanan Bacan adalah:",
+        answer: "D. Semua benar",
+        options: [
+            "A. Kerajaan Waigeo",
+            "B. Kerajaan Rumbati",
+            "C. Kerajaan Namatota",
+            "D. Semua benar"
+        ]
+    },
+    {
+        numb: 25,
+        question: "Salah satu penyebab runtuhnya kerajaan Waigeo adalah:",
+        answer: "D. Semua benar",
+        options: [
+            "A. Serangan VOC",
+            "B. Konflik internal",
+            "C. Masuknya Belanda ke Papua",
+            "D. Semua benar"
+        ]
+    }
+];
